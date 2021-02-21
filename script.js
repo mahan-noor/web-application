d17b2dd62b6051050f262db289189d9fda746948
mahan-noor <ismahanabey@gmail.com>
2021-02-21 15:42:59 +0300

    add index.html change

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    function day(YY, MM, DD) {
+        return((((cc / 4)-2 * cc -1)+((5 * YY /4))+((26 * (MM + 1) / 10)) + DD % 7) + 1
+    }
+    /* alert(result); */
+    var maleAkan = ["Kwasi", "Kwandwo", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"]
+}
