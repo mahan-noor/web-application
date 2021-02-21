7d39f7ea842fe74ba208eade576d55d424529a98
mahan-noor <ismahanabey@gmail.com>
2021-02-21 18:18:03 +0300

    add index.html change

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function myFunction() {
-    function day(YY, MM, DD) {
-        return((((cc / 4)-2 * cc -1)+((5 * YY /4))+((26 * (MM + 1) / 10)) + DD % 7) + 1
+function naming() {
+    function day(YY,MM,DD){
+       return(((5*YY/4) ) + ( (26*(MM+1)/10) + DD) % 7) + 1
+        
     }
+    var YY=parseInt(prompt("Enter the year"));
+    var MM=parseInt(prompt("Enter the month"));
+    var DD=parseInt(prompt("Enter the date"));
     /* alert(result); */
     var maleAkan = ["Kwasi", "Kwandwo", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleAkan =["Akosua" , "Adwoa" , "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "sartuday"];
     if (gender == "male") {
-        alert("YOur aka")
+        alert("Your akan name is " +maleAkan[result] + " you were born on a " + days[result]);
+
+    } else if (gender== "female") {
+        alert ("Your akan name is " +femaleAkan[result] + " you were born on a " + days[result]);
+    } else {
+        alert("null")
     }
 }
