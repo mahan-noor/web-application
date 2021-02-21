6f6a7aae3d109a9646cb43e423d3871e9592905c
mahan-noor <ismahanabey@gmail.com>
2021-02-21 17:45:06 +0300

    add index.html change

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ function myFunction() {
         return((((cc / 4)-2 * cc -1)+((5 * YY /4))+((26 * (MM + 1) / 10)) + DD % 7) + 1
     }
     /* alert(result); */
-    var maleAkan = ["Kwasi", "Kwandwo", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"]
+    var maleAkan = ["Kwasi", "Kwandwo", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleAkan =["Akosua" , "Adwoa" , "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "sartuday"];
+    if (gender == "male") {
+        alert("YOur aka")
+    }
 }
